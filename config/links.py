b2ee56c96c2e072b65d3ec864157b17a61f6ee34
Valery <horrorofabyss@gmail.com>
2024-03-27 17:59:32 +0500

    change host url

--- config/links.py
+++ config/links.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 class Links():
 
-    HOST = "https://dev.qosyl.kz"
+    HOST = "https://vcs.qosyl.kz"
     LOGIN_PAGE = f"{HOST}/login"
     MEET_PAGE = f"{HOST}/meeting/index.html"
     CONNECT_TO_MEETING_PAGE = f"{HOST}/meeting/"
